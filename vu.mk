dae9652ea2e50d1939f5425f2532c5cccde31455
Andreas Fried <AndrolinRoot@googlemail.com>
2015-12-09 20:58:27 +0100

    #VU

--- vu.mk
+++ vu.mk
@@ -17,10 +17,10 @@
 $(call inherit-product, device/samsung/trltexx/full_trltexx.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/vu/config/nfc_enhanced.mk)
 
 # Inherit common CM phone.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/vu/config/common_full_phone.mk)
 
 PRODUCT_DEVICE := trltexx
-PRODUCT_NAME := cm_trltexx
+PRODUCT_NAME := vu_trltexx
